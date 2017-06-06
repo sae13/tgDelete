40e6d0cf92d8905cfb3a0a6ef68841888b58cc6c
saeb molaee <saeb.m.13@gmail.com>
2017-06-06 09:45:07 +0430

    deleted farsi comments

--- tgDelete.py
+++ tgDelete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # coding=utf-8
-#فراخوانی پکیج‌هایی که نیاز داریم
 from __future__ import unicode_literals
 from pytg.receiver import Receiver  # get messages
 from pytg.sender import Sender  # send messages, and other querys.
@@ -13,7 +12,6 @@ inline_image = False
 receiver = tg.receiver
 sender = tg.sender
 __author__ = 'saeb'
-#هرشخص یه یونیک آی دی داره.یه لیست از مدیران تعریف میکنیم که بدونه اینا مدیرن و مجازن
 ADMIN_IDS =['$0100000084cba20ed401c9a0893540b90']  # you should probably change this.
 SpammerUsers=[]
 msgToFirst = "سلام همکار گرامی.\n\
@@ -32,30 +30,24 @@ shahrbabak@nicico.com\n\
 با چهارشماره ای ۶۳۲۹\n\
 با تشکر"
 
-#ارتباتمون رو با تلگرام راه میندازیم و تابعمون فرا میخونیم
 def main():
     receiver.start()
     receiver.message(schedulerDeleter(sender))
     receiver.stop()
     print("I am done")
 @coroutine
-#تابعی که قراره پیاما رو پاک کنه تعریف میکنیم
 def schedulerDeleter(sender):
     quit = False
     try:
         while not quit:
-			#پیام رو از تلگرام به صورت فایل جیسون میگیره
             msg = (yield)
             #print(msg) we need this for debug maybe
-			#وقتی پیامی میاد استاتوس ربات رو آنلاین میکنه
             sender.status_online()
-			#خط زیر میگیم اگه اتفاقی ک الان افتاد از نوع پیام هست-مثلا اگه ما رو توی گروهی
-			#دعوت میکنن اتفاق خاصی نیفته- اگه از نوع پیام هست بعد این کارا رو بکن
             if msg.event == "message" and not msg.own:
-                msgID = msg.id #هرپیام یه آی دی مخصوص به خودش داره
-                msgSenderId = msg.sender.id #هرفرستنده یه آی دی مخصوص به خودش داره
-                msgTime = localtime(time()) #ساعت لوکال سیستم
-                msgHour = int(msgTime[3]) #ما فقط به ساعتش نیاز داریم نه دقیقه
+                msgID = msg.id
+                msgSenderId = msg.sender.id
+                msgTime = localtime(time())
+                msgHour = int(msgTime[3])ه
                 #print (msgTime) for debug
                 # print("\n msg time= {}\n msgID = {}\
                 # msgSenderId = {}".format(msgHour, msgID,\
@@ -68,21 +60,15 @@ def schedulerDeleter(sender):
                 #sender.send_msg(msg.sender.cmd,"hi")
                 if (msg.sender.id not in ADMIN_IDS) and \
                 (( msgHour > 0 ) or ( msgHour < 7 )):
-#بالا گفتی اگه فرستنده از لیست مدیران نیست و ساعت پیام بین یازده شب تا هفت صبحه
                     if msgSenderId not in SpammerUsers:
                         SpammerUsers.append(msgSenderId)
                         sender.send_msg(msgSenderId, msgToFirst)
                         sender.send_msg(msgSenderId, msgToBtw)
                     sender.fwd(msgSenderId,msgID)
                     sender.message_delete(msgID)
-					#به طرف پیامای پیش فرض رو که تعریف کردیم میده بعد پیامی فرستاده
-					#بعد پیامی که خودش فرستاده رو به خودش فوروارد میکنه
-					#و پیام رو پاک میکنه
 
                 else:
                     continue
-            #خط‌های زیر ادامه دستور ترای هست برای وقتایی که دستورات ارضا نمی‌شن و تعریف
-			#کلید خروچ
 
     except GeneratorExit:
 
@@ -93,6 +79,5 @@ def schedulerDeleter(sender):
     else:
 
         pass
-#اگه تابع ماین داخل همین صفحه کد تعریف شده اجراش کن
 if __name__ == '__main__':
     main()
